1acdcf644c8eeb68fdd076bf52be45267ef3e907
Akhmadjanov Anvar <www.latveinhoasadrizeges@gmail.com>
2024-04-05 05:51:42 +0500

    feat: added song

--- src/app/modules/home/home-page/home-page.component.ts
+++ src/app/modules/home/home-page/home-page.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {MatAnchor} from '@angular/material/button';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
+    selector: 'app-home-page',
+    standalone: true,
     imports: [
         RouterLink,
         MatAnchor
     ],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
 }
